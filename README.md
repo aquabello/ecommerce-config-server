b84e1cb24ea29e5d35ce830e3c781fe1464cf5dc
aquabello <aquabello@nhnenter.com>
2023-02-26 12:59:02 +0900

    [20220226] config server setting

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ecommerce-config-server
+
+## Config 호출
+
+## 기본
+http://localhost:8888/ecommerce/default
--- src/main/java/com/today25/ecommerceconfigserver/EcommerceConfigServerApplication.java
+++ src/main/java/com/today25/ecommerceconfigserver/EcommerceConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.today25.ecommerceconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class EcommerceConfigServerApplication {
 
     public static void main(String[] args) {
