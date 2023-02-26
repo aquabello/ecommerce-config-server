7fc3770975a4b44df05ddf381576cdb9aea44543
aquabello <aquabello@nhnenter.com>
2023-02-26 12:47:47 +0900

    first commit

---
+++ src/main/java/com/today25/ecommerceconfigserver/EcommerceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.today25.ecommerceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceConfigServerApplication.class, args);
+    }
+
+}
